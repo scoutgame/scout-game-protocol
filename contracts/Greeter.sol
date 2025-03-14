45c304ae660cf64213b428abc1c096cccb6f29dd
Matt Casey <mattwad@gmail.com>
2025-03-14 14:13:27 -0600

    add starter packs to deploy script (#37)
    * moved starter pack nfts inside protocol contracts
    
    * fixed broken imports
    
    * Starter pack nft contracts
    
    * Fixed broken tests and type issues
    
    * move starter pack contracts back
    
    * move clients back
    
    * delete implementation
    
    * fix implementation
    
    ---------
    
    Co-authored-by: Safwan Shaheer <devorein00@gmail.com>

--- contracts/Greeter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.26;
-
-contract Greeter {
-    function hello() public pure returns (string memory) {
-        return "Hello, World!";
-    }
-}
